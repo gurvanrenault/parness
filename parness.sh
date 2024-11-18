b291d0b233ec2a8cda5d6f16c216786247af4461
gurvanrenault <renault.gurvan@live.com>
2024-11-18 17:44:14 +0100

    Sanitize input from user

--- parness.sh
+++ parness.sh
@@ -1,37 +1,72 @@
-#!/bin/bash
+#!/bin/sh
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 BLANK='\033[0;37m'
 
+isCommandFound=false
+isParameterCheckFound=false
 
-#TODO :  Check  parameters numbers
+list_commands="check"
+list_parameters_check="dns all proxy deamon"
 
 echo "${BLANK}"
-# Check if  the DNS configured on your system  is legit
-if [ "$1" = "check" ] && ([ "$2" = "dns" ] || [ "$2" = "all" ]); then
-	echo  "Check DNS in whitelist ... "
-	#Script Python pour vérifier une whitelist dns
-	DNSVALUE=$(sudo grep -Eo '([0-9]+\.)+[0-9]+' /etc/resolv.conf)
-        echo "DNS value found :  $DNSVALUE"
-	if grep -q $DNSVALUE whitelistDNS ; then
-           echo "${GREEN}DNS value found in whitelist ${BLANK}"
-	else
-           echo -e "${RED}DNS value seems altered and/or malicious ${BLANK}"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Sanitize input from user 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if  [ $# -lt 2 ]; then
+ echo "AAAAAAA"; 
+ cat  README.md
+else
+   
+    for command in $list_commands; do 
+        if [ "$command" = "$1" ]; then 
+           isCommandFound=true 
+           break 
+      fi 
+    done
+
+    if [ "$1" = "check" ]; then
+        for  parameter in $list_parameters_check; do 
+            if [ "$parameter" = "$2" ]; then 
+                isParameterCheckFound=true
+                break 
+            fi 
+        done
+       
+        echo "isParameterCheckFound : $isParameterCheckFound"
+        if [ "$isParameterCheckFound" = "false" ];then
+           
+            cat README.md 
+
         fi
+    fi
 fi
-if [ "$1" = "check" ] && ([ "$2" = "proxy" ] || [ "$2" = "all" ]); then
-        if [ ! -z $http_proxy ]; then
-	   echo "Current HTTP proxy is $http_proxy ..."
-        else
-           echo "HTTP proxy is not configured"
-	fi
-        if [ ! -z $https_proxy ]; then
-           echo "Current HTTPS proxy is $https_proxy ..."
-        else
-           echo "HTTPS proxy is not configured ..."
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if [ isCommandFound ]; then
+        if [ "$1" = "check" ] && ([ "$2" = "dns" ] || [ "$2" = "all" ]); then
+                echo  "Check DNS in whitelist ... "
+                DNSVALUE=$(sudo grep -Eo '([0-9]+\.)+[0-9]+' /etc/resolv.conf)
+                echo "DNS value found :  $DNSVALUE"
+                if grep -q $DNSVALUE whitelistDNS ; then
+                echo "${GREEN}DNS value found in whitelist ${BLANK}"
+                else
+                echo -e "${RED}DNS value seems altered and/or malicious ${BLANK}"
+                fi
+        fi 
+        if [ "$1" = "check" ] && ([ "$2" = "proxy" ] || [ "$2" = "all" ]); then
+                if [ ! -z $http_proxy ]; then
+                echo "Current HTTP proxy is $http_proxy ..."
+                else
+                echo "HTTP proxy is not configured"
+                fi
+                if [ ! -z $https_proxy ]; then
+                echo "Current HTTPS proxy is $https_proxy ..."
+                else
+                echo "HTTPS proxy is not configured ..."
+                fi
         fi
-fi
-if [ "$1" = "check" ] && ([ "$2" = "deamon" ] ||  [ "$2" = "all" ]); then
-	echo  "Listing all deamons ... "
-        systemctl | grep daemon
+        if [ "$1" = "check" ] && ([ "$2" = "deamon" ] ||  [ "$2" = "all" ]); then
+                echo  "Listing all deamons ... "
+                systemctl | grep daemon
+        fi
 fi
